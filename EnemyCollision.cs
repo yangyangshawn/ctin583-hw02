ce1e6cf15a71c458d3703f7800cfbc5839848c01
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-01-16 22:33:18 -0800

    add project homework files for all problems

---
+++ EnemyCollision.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    // TODO: Problem 2: Call the Movement and Shoot functions.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    void Movement ()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+    
+    
+    void Shoot ()
+    {
+        if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            myStuff.bullets--;
+        }
+    }
+
+    // TODO: Problem 3: Define a public class named Weapon. In the class, define 3 int variables for arrow, sword, and rocket.
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,9 +1,11 @@
+// TODO: Problem 4: Write in the comments what each line of code is doing
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // TODO: Problem 1: Define a private variable for your player or character's name and print it out to Unity's console
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    [Header("General Setup Settings")]
+    [SerializeField] private InputAction movement;
+    [Tooltip("How fast player moves up and down based upon player input")][SerializeField] float controlSpeed = 30f;
+    [Tooltip("How far player moves horizontally")][SerializeField] float xRange = 10f;
+    [Tooltip("How far player moves vertically")][SerializeField] float yRange = 10f;
+
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
     }
 }
